1f72225fd20e257d406fc0162214be072e9963e6
Chris Busillo <info@shinycomputers.com>
2026-06-21 11:11:15 -0400

    Scope visionOS widget dark appearance

--- Sources/ContextPanelWidgetUI/ContextPanelWidgetViews.swift
+++ Sources/ContextPanelWidgetUI/ContextPanelWidgetViews.swift
@@ -801,13 +801,9 @@ public enum CPWTheme {
     }
     #elseif canImport(UIKit)
     private static func adaptiveColor(light: UIColor, dark: UIColor) -> Color {
-        #if os(visionOS)
-        Color(uiColor: dark)
-        #else
         Color(uiColor: UIColor { traits in
             traits.userInterfaceStyle == .dark ? dark : light
         })
-        #endif
     }
     #else
     private static func adaptiveColor(light: Color, dark: Color) -> Color {
